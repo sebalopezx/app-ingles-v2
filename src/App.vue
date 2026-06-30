491102c4bd6611b51114d6c8c5175ba5dceab7b8
Sebastian Lopez <se.lopezn@gmail.com>
2026-06-30 17:30:14 -0400

    Add bg layout

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
 <script setup>
 import AppHeader from "./components/Header.vue";
 import AppFooter from "./components/Footer.vue";
+
+const backgroundImage = `url("${import.meta.env.BASE_URL}fondo_ingles.jpg")`;
 </script>
 
 <template>
-  <div class="app">
-    <AppHeader />
+  <div class="app-shell">
+    <div
+      class="app-bg"
+      :style="{ backgroundImage }"
+      aria-hidden="true"
+    ></div>
 
-    <main>
-      <RouterView v-slot="{ Component }">
-        <Transition name="page" mode="out-in">
-          <component :is="Component" />
-        </Transition>
-      </RouterView>
-    </main>
+    <div class="app">
+      <AppHeader />
 
-    <AppFooter />
+      <main>
+        <RouterView v-slot="{ Component }">
+          <Transition name="page" mode="out-in">
+            <component :is="Component" />
+          </Transition>
+        </RouterView>
+      </main>
+
+      <AppFooter />
+    </div>
   </div>
 </template>
